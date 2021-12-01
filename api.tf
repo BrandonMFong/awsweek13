6f505c9072dc625522a35240256384c65172f746
BrandonMFong <bfong@owc.com>
2021-12-01 05:54:15 +0000

    Creating routes

--- api.tf
+++ api.tf
@@ -11,3 +11,20 @@ resource "aws_apigatewayv2_integration" "week13-integration" {
   payload_format_version = "2.0"
 }
 
+resource "aws_apigatewayv2_route" "week13-get-route" {
+  api_id    = aws_apigatewayv2_api.week13-apigw.id
+  route_key = "GET /urls/{id}"
+
+  target = "integrations/${aws_apigatewayv2_integration.week13-integration.id}" 
+}
+
+resource "aws_apigatewayv2_route" "week13-post-route" {
+  api_id    = aws_apigatewayv2_api.week13-apigw.id
+  route_key = "POST /urls"
+
+  target = "integrations/${aws_apigatewayv2_integration.week13-integration.id}" 
+}
+
+output "week13-apigw-endpoint" {
+  value = aws_apigatewayv2_api.week13-apigw.api_endpoint
+}
